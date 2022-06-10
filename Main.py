f99fc5d1fb510af79b2df0f1edc0619ecd232144
Mrleonard <listas036@gmail.com>
2022-06-10 16:00:27 -0300

    V_1.5

--- Main.py
+++ Main.py
@@ -7,11 +7,13 @@ u = s = None
 file = open('Logs.txt', 'r+')
 contas = file.readlines()
 
+# Introdução
 print('Ola tester!')
 sleep(1)
 c = str(input('Ja possui uma conta? [S/N] '))
 sleep(1)
 
+# Se não possiu uma conta
 if c.lower() == 'n':
     print('cadastre-se por favor\n')
     sleep(1)
@@ -19,8 +21,10 @@ if c.lower() == 'n':
     s = str(input('Senha: '))
     login = f'{u} {s}\n'
 
+    # Anota no arquivo
     file.write(login)
 
+# Se tem conta
 if c.lower() == 's':
     print('Por favor identifique-se\n')
     sleep(1)
@@ -28,7 +32,9 @@ if c.lower() == 's':
     s = str(input('Senha: '))
     login = f'{u} {s}'
 
+    # Enquanto o contador for menor do que a list
     while count < len(contas):
+        # Faz a comparação
         if login == contas[count].replace('\n', ''):
             log = True
             break
@@ -37,6 +43,7 @@ if c.lower() == 's':
 
         count += 1
 
+# Conclusão
 if log:
     print(f'[Login concluido]')
 
